cf8e47ffdc1a5970615e1116a210714a4e96a47b
LudwigPosn1 <ludwigpoisson2105@gmail.com>
2026-05-16 12:14:37 +0200

    fichier .env pour que ça pointe sur la VMAzure

--- constants/api.ts
+++ constants/api.ts
@@ -3,4 +3,4 @@
  * REMPLACEZ '192.168.1.XX' par l'adresse IP locale de votre ordinateur (IPv4).
  * Vous pouvez la trouver avec 'ipconfig' (Windows) ou 'ifconfig' (Mac/Linux).
  */
-export const API_URL = 'http://192.168.0.22:8000'; // Par défaut, gardons celle-ci mais l'utilisateur devra la changer
+export const API_URL = 'http://9.223.112.208:8000'; // Par défaut, gardons celle-ci mais l'utilisateur devra la changer
